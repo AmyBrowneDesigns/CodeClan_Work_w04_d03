a683eebeb12c83a47bee805fe8f50ec00f4e95ca
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-30 13:56:03 +0100

    pre-lunch updates to files.

--- 01_full_stack_task_list/start_point/controllers/tasks_controller.py
+++ 01_full_stack_task_list/start_point/controllers/tasks_controller.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+
 from repositories import task_repository
+from repositories import user_repository
+from models.task import Task
 
 from flask import Blueprint
 
@@ -19,19 +22,34 @@ def tasks():
 # GET '/tasks/new'
 @tasks_blueprint.route("/tasks/new")
 def new_task():
-    return render_template("/tasks/new.html")
+    users = user_repository.select_all()
+    return render_template("/tasks/new.html", all_users=users)
 
 
 # CREATE
 # POST '/tasks'
+@tasks_blueprint.route("/tasks", methods=["POST"])      #default is ["GET"] so doesnt need to be decalred for get.
+def create_task():
+    description = request.form["description"]
+    user_id = request.form["user_id"]
+    duration = request.form["duration"]
+    completed = request.form["completed"]
+    user = user_repository.select(user_id)
 
-# SHOW
+    task = Task(description, user, duration, completed)
+
+    task_repository.save(task)
+
+    return redirect('/tasks')
+
+
+# SHOW                                                  # for showing one thing in detail. {{task.description}} in the html can be placed inside an <a></a> tag
 # GET '/tasks/<id>'
 
-# EDIT
+# EDIT                                                  # slightly harder code
 # GET '/tasks/<id>/edit'
 
-# UPDATE
+# UPDATE                                                # slightly harder code
 # PUT '/tasks/<id>'
 
 # DELETE
